db36c757a0d695ccac0aaf602fd100891eccac79
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-13 15:06:09 -0300

    Rewrite rules with nonsolitary terminals

--- lib/context_free_language_recognition.ex
+++ lib/context_free_language_recognition.ex
@@ -12,21 +12,32 @@ defmodule ContextFreeLanguageRecognition do
     end
   end
 
-  def build_nonsolitary_terminal_rule(terminal) do
-    {}
-  end
-
   def terminal_alias_nonterminal(terminal) do
-    {"N_" <> String.upcase(terminal), terminal}
+    "N_" <> String.upcase(terminal)
   end
 
-  def eliminate_nonsolitary_terminal({rules, start}) do
+  def terminal_alias_nonterminal_rule(terminal) do
+    {terminal_alias_nonterminal(terminal), [terminal]}
+  end
+
+  def replace_solitary_terminals(rule_right) do
+    Enum.map(rule_right, &(
+      if is_terminal(&1) do
+        terminal_alias_nonterminal(&1)
+      else
+        &1
+      end
+    ))
+  end
+
+  def eliminate_nonsolitary_terminal({rules, _}) do
     Enum.reduce(rules, MapSet.new, fn({rule_left, rule_right}, acc) ->
       right_terminals = Enum.filter(rule_right, &is_terminal(&1))
       if right_terminals == [] or length(rule_right) <= 1 do
         MapSet.put(acc, {rule_left, rule_right})
       else
-        MapSet.put(acc, )
+        Enum.reduce(right_terminals, acc, &MapSet.put(&2, terminal_alias_nonterminal_rule &1))
+        |> MapSet.put({rule_left, replace_solitary_terminals rule_right})
       end
     end)
   end
--- test/context_free_language_recognition_test.exs
+++ test/context_free_language_recognition_test.exs
@@ -20,6 +20,10 @@ defmodule ContextFreeLanguageRecognitionTest do
     {"MUL", ["/"]},
   ]
 
+  def assert_same_content(a, b) do
+    assert MapSet.new(a) == MapSet.new(b)
+  end
+
   test "checks for terminals" do
     assert is_terminal("+")
     assert is_terminal("a")
@@ -27,6 +31,7 @@ defmodule ContextFreeLanguageRecognitionTest do
     assert not is_terminal("A")
     assert not is_terminal("VB")
     assert not is_terminal("N_A")
+    assert not is_terminal("N_+")
   end
 
   test "eliminates start symbols" do
@@ -44,6 +49,29 @@ defmodule ContextFreeLanguageRecognitionTest do
   end
 
   test "aliases terminals as nonterminals" do
-    assert terminal_alias_nonterminal("+") == {"N_+", "+"}
+    assert terminal_alias_nonterminal_rule("+") == {"N_+", ["+"]}
+  end
+
+  test "replaces solitary terminals" do
+    assert replace_solitary_terminals(["X", "+", "a"]) == ["X", "N_+", "N_A"]
+  end
+
+  test "rewrites rules with nonsolitary terminals" do
+    assert_same_content(eliminate_nonsolitary_terminal({[
+      {"A", ["B"]},
+      {"A", ["a"]},
+      {"B", ["b"]},
+      {"B", ["B", "b"]},
+      {"B", ["b", "+", "b"]},
+    ], nil}),
+    [
+      {"A", ["B"]},
+      {"A", ["a"]},
+      {"B", ["b"]},
+      {"N_B", ["b"]},
+      {"B", ["B", "N_B"]},
+      {"N_+", ["+"]},
+      {"B", ["N_B", "N_+", "N_B"]},
+    ])
   end
 end
