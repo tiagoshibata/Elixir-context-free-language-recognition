473a17103591d97d652f293744122335da23d21b
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-12 23:08:49 -0300

    Start Chomsky normal form conversion
    Eliminate start symbols from right side of rules

--- lib/context_free_language_recognition.ex
+++ lib/context_free_language_recognition.ex
@@ -1,18 +1,38 @@
 defmodule ContextFreeLanguageRecognition do
-  @moduledoc """
-  Documentation for ContextFreeLanguageRecognition.
-  """
+  def atoms_to_string(atoms) do
+    Enum.map(atoms, &Atom.to_string(&1))
+    |> Enum.join()
+  end
 
-  @doc """
-  Hello world.
+  def is_terminal(atom) do
+    string = Atom.to_string(atom)
+    not (String.downcase(string) != string)
+  end
 
-  ## Examples
+  def eliminate_start({rules, start}) do
+    if Enum.map(rules, &(Enum.member?(elem(&1, 1), start)))
+    |> Enum.any? do
+      {MapSet.put(rules, {:S0, start}), :S0}
+    else
+      {rules, start}
+    end
+  end
 
-      iex> ContextFreeLanguageRecognition.hello
-      :world
+  def build_nonsolitary_terminal_rule(terminal) do
+    {}
+  end
 
-  """
-  def hello do
-    :world
+  def eliminate_nonsolitary_terminal({rules, start}) do
+    Enum.reduce(rules, MapSet.new, fn({rule_left, rule_right}, acc) ->
+      right_terminals = Enum.filter(rule_right, &is_terminal(&1))
+
+      MapSet.put(acc, )
+    )
+    if Enum.map(rules, &(Enum.member?(elem(&1, 1), start)))
+    |> Enum.any? do
+      {[{:S0, start} | rules], :S0}
+    else
+      {rules, start}
+    end
   end
 end
--- test/context_free_language_recognition_test.exs
+++ test/context_free_language_recognition_test.exs
@@ -1,8 +1,49 @@
 defmodule ContextFreeLanguageRecognitionTest do
   use ExUnit.Case
+  import ContextFreeLanguageRecognition
   doctest ContextFreeLanguageRecognition
 
-  test "greets the world" do
-    assert ContextFreeLanguageRecognition.hello() == :world
+  @math_expression [
+    {:EXPR, [:TERM]},
+    {:EXPR, [:EXPR, :ADD, :TERM]},
+    {:EXPR, [:ADD, :TERM]},
+    {:TERM, [:FACTOR]},
+    {:TERM, [:TERM, :MUL, :FACTOR]},
+    {:FACTOR, [:PRIMARY]},
+    {:FACTOR, [:FACTOR, :"^", :PRIMARY]},
+    {:PRIMARY, [:number]},
+    {:PRIMARY, [:variable]},
+    {:PRIMARY, [:"(", :EXPR, :")"]},
+    {:ADD, [:"+"]},
+    {:ADD, [:"-"]},
+    {:MUL, [:"*"]},
+    {:MUL, [:"/"]},
+  ]
+
+  test "joins atoms to string" do
+    assert atoms_to_string([:e, :l, :i, :x, :i, :r]) == "elixir"
+  end
+
+  test "checks for terminals" do
+    assert is_terminal(:"+")
+    assert is_terminal(:a)
+    assert is_terminal(:"0")
+    assert not is_terminal(:"A")
+    assert not is_terminal(:"VB")
+    assert not is_terminal(:"N_A")
+  end
+
+  test "eliminates start symbols" do
+    grammar = {[], :S}
+    assert eliminate_start(grammar) == grammar
+
+    rules = [{:S, [:A]}, {:A, [:a]}, {:A, [:B, :C]}, {:B, [:b]}, {:C, [:c]}]
+    grammar = {rules, :S}
+    # Should be unchanged if no start symbols in the right side
+    assert eliminate_start(grammar) == grammar
+
+    # Should work with MapSet
+    rules = MapSet.new [{:S, [:A]}, {:A, [:S, :A]}, {:A, [:a]}]
+    assert eliminate_start({rules, :S}) == {MapSet.put(rules, {:S0, :S}), :S0}
   end
 end
