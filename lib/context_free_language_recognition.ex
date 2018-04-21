c8caf4d06a263412c3456334b9ad1d4691b85c93
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-04-21 12:27:26 -0300

    Implement CKY

--- lib/context_free_language_recognition.ex
+++ lib/context_free_language_recognition.ex
@@ -132,4 +132,42 @@ defmodule ContextFreeLanguageRecognition do
     |> eliminate_unit_rules
     {rules, start}
   end
+
+  def init_cky({rules, _}, sentence) do
+    first_expansion = Enum.reverse(sentence)
+    |> Enum.reduce([], fn(c, acc) ->
+      rules_to_c = Enum.filter(rules, &(elem(&1, 1) == [c]))
+      |> Enum.map(&elem(&1, 0))
+      |> MapSet.new
+      [rules_to_c | acc]
+    end)
+
+    n = length(sentence)
+    [first_expansion] ++ (
+      List.duplicate(MapSet.new, n)
+      |> List.duplicate(n - 1)
+    )
+  end
+
+  def cky({rules, start}, sentence) do
+    m = init_cky({rules, start}, sentence)
+    n = length(sentence)
+    nt_rules = Enum.filter(rules, &(length(elem(&1, 1)) == 2))
+    Enum.reduce(2..n, m, fn(l, acc_l) ->
+      Enum.reduce(0..(n - l), acc_l, fn(s, acc_s) ->
+        Enum.reduce(0..l - 2, acc_s, fn(p, acc_p) ->
+          Enum.reduce(nt_rules, acc_p, fn({a, [b, c]}, acc) ->
+            if MapSet.member?(Enum.at(acc, p) |> Enum.at(s), b) and MapSet.member?(Enum.at(acc, l - p - 2) |> Enum.at(s + p + 1), c) do
+              List.update_at(acc, l, fn(sublist) -> List.update_at(sublist, s, &MapSet.put(&1, a)) end)
+            else
+              acc
+            end
+          end)
+        end)
+      end)
+    end)
+    |> Enum.at(n - 1)
+    |> Enum.at(0)
+    |> MapSet.member?(start)
+  end
 end
--- test/context_free_language_recognition_test.exs
+++ test/context_free_language_recognition_test.exs
@@ -299,4 +299,34 @@ defmodule ContextFreeLanguageRecognitionTest do
 
     assert to_chomsky_nf({math_rules, "EXPR"}) == {expected_rules, "S0"}
   end
+
+  test "initializes CKY" do
+    cky = init_cky({[
+      {"S", ["a"]},
+      {"A", ["a"]},
+      {"B", ["b"]},
+      {"C", ["c"]},
+    ], nil}, ["a", "b", "a"])
+    assert length(cky) == 3
+    assert Enum.map(cky, &length/1) == [3, 3, 3]
+    assert cky == [[MapSet.new(["S", "A"]), MapSet.new(["B"]), MapSet.new(["S", "A"])] | List.duplicate(MapSet.new, 3) |> List.duplicate(2)]
+  end
+
+  test "runs CKY" do
+    rules = [
+      {"S", ["NP", "VP"]},
+      {"VP", ["VP", "PP"]},
+      {"VP", ["V", "NP"]},
+      {"VP", ["eats"]},
+      {"PP", ["P", "NP"]},
+      {"NP", ["DET", "N"]},
+      {"NP", ["she"]},
+      {"V", ["eats"]},
+      {"P", ["with"]},
+      {"N", ["fish"]},
+      {"N", ["fork"]},
+      {"DET", ["a"]},
+    ]
+    assert cky({rules, "S"}, ["she", "eats", "a", "fish", "with", "a", "fork"])
+  end
 end
